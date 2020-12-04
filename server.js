cd446e49c6e83b38dd40670f80cdd5fcdc6e69ab
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-12-03 22:35:27 -0500

    added fuction to update employee

--- server.js
+++ server.js
@@ -11,20 +11,22 @@ var connection = mysql.createConnection({
   database : 'employeeTracker_db'
 });
  
-connection.connect();
- 
 // establishing connection
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+    
+  });
 
-figlet("Employee Manager", function (err, data) {
+figlet("Employee Manager",  (err, data) => {
     if (err) {
       console.log("Issue");
       console.dir(err);
       return;
     }
+console.log (data);
+
+});
 
 // prompts for user 
 const promptStart = () => {
@@ -46,8 +48,8 @@ const promptStart = () => {
           ],
         },
       ])
-      .then(({ selection }) => {
-        switch (selection) {
+      .then(({ input }) => {
+        switch (input) {
           case "Add a new Department":
             addDept();
             break;
@@ -75,9 +77,8 @@ const promptStart = () => {
             console.log("Have a great day!");
         }
       });
-  }
-
-//   function to add employee 
+    }
+  //   function to add employee 
 
   addEmployee = () => {
     console.log("Adding a New Employee");
@@ -149,10 +150,10 @@ addRole = () => {
   };
 
 //   function for adding new department 
-  addDepartment = () => {
-    console.log("Adding a department.");
-    return inquirer
-      .prompt([
+addDepartment = () => {
+console.log("Adding a department.");
+return inquirer
+    .prompt([
         {
           type: "input",
           name: "departmentName",
@@ -175,10 +176,10 @@ addRole = () => {
 //   function to view all employees 
 
 Employees = () => {
-    console.log("View All Employees");
-    connection.query("select * from employee;", response, (err, res) => {
-      if (err) throw err;
-      console.table(res);
+console.log("View All Employees");
+connection.query("select * from employee;", response, (err, res) => {
+if (err) throw err;
+console.table(res);
       
       promptStart();
     });
@@ -186,11 +187,11 @@ Employees = () => {
   
 //   view all departments
   Department = () => {
-    console.log("View All Employees by Department");
-    connection.query("SELECT * FROM department;", response, (err, res) => {
-      if (err) throw err;
-      cTable(res);
-      promptUser();
+  console.log("View All Employees by Department");
+  connection.query("SELECT * FROM department;", response, (err, res) => {
+  if (err) throw err;
+  cTable(res);
+  promptUser();
     });
   };
 
@@ -200,6 +201,56 @@ Employees = () => {
       if (err) throw err;
       const table = cTable.getTable(res);
       console.log(table);
-      userOptions();
     });
-  connection.end();
+
+    function updateEmployeeRole() {
+        let newRole = {};
+      
+    connection.query(
+    "SELECT employee.id, employee.firstName, employee.lastName, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee AS e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id",
+    (err, res) => {
+     if (err) throw err;
+    inquirer
+    .prompt([
+                
+        {
+        name: "updateEmployee",
+        type: "list",
+        message: "Which employee would you like to update?",
+        choices: function () {
+        let choiceArray = [];
+        for (var i = 0; i < res.length; i++) {
+        choiceArray.push(res[i].first_name);
+        }
+            return choiceArray;
+                  },
+                },
+              ])
+        .then((userInput) => {
+        newEmployee.firstName = userInput.firstName;
+        newEmployee.lastName = userInput.lastName;
+                
+        connection.query(
+        "SELECT * FROM role WHERE title = ?",
+        userInput.rolechoice,
+        (err, res) => {
+        if (err) throw err;
+        
+        newEmployee.role_id = res[0].id;
+
+        connection.query(
+        "UPDATE employee SET role_id = ? WHERE first_name = ?",
+        [newRole.role_id, newRole.first_name],
+        (err, res) => {
+        if (err) throw err;
+        }
+        );
+    }
+        );
+
+});
+    });
+};
+
+  }
+
